68254c7c0b52b4220e840dbb8fd11d06df134913
Luis Gonzalez <luis.gonzalez@amadeus.com>
2025-11-16 16:46:11 -0700

    updating strategies

--- CodingGame/CelulArena.cs
+++ CodingGame/CelulArena.cs
@@ -67,28 +67,158 @@ using System.Collections.ObjectModel;
 //     Output
 //     A single line with your action: GROW id x y type : attempt to grow a new organ of type type at location x, y from organ with id id. If the target location is not a neighbour of id, the organ will be created on the shortest path to x, y.
 
-internal class Arena
+public class Arena
 {
+    public readonly int HEIGHT;
+    public readonly int WIDTH;
+
+    private Entity[] _entities;
+
+
     private Node[] grid;
-    private int HEIGHT;
-    private int WIDTH;
+
+    public Entity[] MyOrgans;
+    public int Owner_Me = 1;
+    public Node[] Resources;
 
     public Arena(int width, int height)
     {
         WIDTH = width;
         HEIGHT = height;
     }
+
+    public ReadOnlyCollection<Node> Grid => grid.AsReadOnly();
+
+    public void UpdateArenaWithEntities(Entity[] entities)
+    {
+        _entities = entities;
+        MyOrgans = entities.Where(e => e.Owner == Owner_Me).ToArray();
+
+        grid = new Node[WIDTH * HEIGHT];
+        for (var i = 0; i < WIDTH * HEIGHT; i++)
+            grid[i] = new Node
+            {
+                X = i % WIDTH,
+                Y = i / WIDTH,
+                Type = NodeType.EMPTY,
+                Occupier = null
+            };
+
+        foreach (var entity in entities)
+        {
+            var index = entity.Y * WIDTH + entity.X;
+            grid[index].Occupier = entity;
+            switch (entity.EntityType)
+            {
+                case EntityType.WALL:
+                    grid[index].Type = NodeType.WALL;
+                    break;
+                case EntityType.A:
+                case EntityType.B:
+                case EntityType.C:
+                case EntityType.D:
+                    grid[index].Type = NodeType.PROTEIN_SOURCE;
+                    break;
+                default:
+                    grid[index].Type = NodeType.ORGANISM;
+                    break;
+            }
+        }
+
+        Resources = UpdateResourceNodes();
+    }
+
+    public Node[] GetNodesICanGrowInto()
+    {
+        var myOrgans = _entities.Where(e => e.Owner == Owner_Me);
+        var growableNodes = new List<Node>();
+
+        foreach (var organ in myOrgans)
+        {
+            var adjacentPositions = new (int X, int Y)[]
+            {
+                (organ.X + 1, organ.Y),
+                (organ.X - 1, organ.Y),
+                (organ.X, organ.Y + 1),
+                (organ.X, organ.Y - 1)
+            };
+
+            foreach (var pos in adjacentPositions)
+            {
+                if (pos.X < 0 || pos.X >= WIDTH || pos.Y < 0 || pos.Y >= HEIGHT)
+                    continue;
+
+                var index = pos.Y * WIDTH + pos.X;
+                var node = grid[index];
+                if (node.Type == NodeType.EMPTY || node.Type == NodeType.PROTEIN_SOURCE) growableNodes.Add(node);
+            }
+        }
+
+        return growableNodes.ToArray();
+    }
+
+    private Node[] UpdateResourceNodes()
+    {
+        return grid.Where(n => n.Type == NodeType.PROTEIN_SOURCE).ToArray();
+    }
+
+    public Node[] SortNodesByDistanceTo(Node targetNode)
+    {
+        return grid.OrderBy(n => Utilities.CalculateDistance(n, targetNode)).ToArray();
+    }
+
+    public Node[] SortByDistanceToNodesICanGrowInto(Node[] nodesICanGrowInto)
+    {
+        return grid.OrderBy(n => nodesICanGrowInto.Min(growNode => Utilities.CalculateDistance(n, growNode))).ToArray();
+    }
+
+    public Node[] SortByDistanceToResourceNodes(Node[] nodesICanGrowInto)
+    {
+        var resourceNodes = UpdateResourceNodes();
+        return grid.OrderBy(n => resourceNodes.Min(resourceNode => Utilities.CalculateDistance(n, resourceNode)))
+            .ToArray();
+    }
 }
 
-internal class Node
+public static class Utilities
 {
-    public int Id;
-    public string Type;
+    public static int CalculateDistance((int X, int Y) a, (int X, int Y) b)
+    {
+        return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+    }
+
+    public static int CalculateDistance(BaseNode a, BaseNode b)
+    {
+        return CalculateDistance((a.X, a.Y), (b.X, b.Y));
+    }
+}
+
+public enum NodeType
+{
+    EMPTY,
+    WALL,
+    ORGANISM,
+    PROTEIN_SOURCE
+}
+
+public abstract class BaseNode
+{
     public int X;
     public int Y;
 }
 
-internal class Entity
+public class Node : BaseNode
+{
+    public Entity Occupier;
+    public NodeType Type;
+
+    public override string ToString()
+    {
+        return $"Node[X={X}, Y={Y}, Type={Type}, Occupier={(Occupier != null ? Occupier.EntityType : "None")}]";
+    }
+}
+
+public class Entity : BaseNode
 {
     public string EntityType;
     public string OrganDir;
@@ -96,8 +226,11 @@ internal class Entity
     public int OrganParentId;
     public int OrganRootId;
     public int Owner;
-    public int X;
-    public int Y;
+
+    public override string ToString()
+    {
+        return $"Entity[X={X}, Y={Y}, Type={EntityType}, Owner={Owner}, OrganId={OrganId}]";
+    }
 }
 
 public static class EntityType
@@ -129,6 +262,11 @@ public class Resources
         C = c;
         D = d;
     }
+}
+
+public class ActionSlot
+{
+    public Action Action { get; set; }
 }
 
 public abstract class Action
@@ -165,33 +303,223 @@ public class GrowAction : Action
     }
 }
 
+public static class DebugLog
+{
+    public static void Log(string message)
+    {
+        Console.Error.WriteLine(message);
+    }
+}
+
+public class Strategies
+{
+    private readonly Arena _arena;
+    private readonly Resources _myResources;
+    private readonly Resources _oppResources;
+    private readonly ActionPlayer _player;
+
+    public Strategies(Resources myResources, Resources oppResources, Arena arena, ActionPlayer player)
+    {
+        _myResources = myResources;
+        _oppResources = oppResources;
+        _arena = arena;
+        _player = player;
+    }
+
+    public Node GetClosestOrganToNode(Node node)
+    {
+        var myOrgans = _arena.MyOrgans;
+
+        Entity closestOrgan = null;
+        var closestDistance = int.MaxValue;
+
+        foreach (var organ in myOrgans)
+        {
+            var distance = Utilities.CalculateDistance(organ, node);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestOrgan = organ;
+            }
+        }
+
+        if (closestOrgan == null) return null;
+
+        return new Node
+        {
+            X = closestOrgan.X,
+            Y = closestOrgan.Y,
+            Occupier = closestOrgan,
+            Type = NodeType.ORGANISM
+        };
+    }
+
+    public bool DoIHaveEnoughResourcesForGrowth()
+    {
+        return _myResources.A >= 1;
+    }
+
+    public Node? GetClosestResourceNodeToAnyOfMyOrgans()
+    {
+        var resourceNodes = _arena.Resources;
+        var myOrgans = _arena.MyOrgans;
+
+        Node closestResourceNode = null;
+        var closestDistance = int.MaxValue;
+
+        foreach (var resourceNode in resourceNodes)
+        foreach (var organ in myOrgans)
+        {
+            var distance = Utilities.CalculateDistance(organ, resourceNode);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestResourceNode = resourceNode;
+            }
+        }
+
+        return closestResourceNode;
+    }
+
+    public Entity GetClosestOrganToResourceNode(Node resourceNode)
+    {
+        var myOrgans = _arena.MyOrgans;
+
+        Entity closestOrgan = null;
+        var closestDistance = int.MaxValue;
+
+        foreach (var organ in myOrgans)
+        {
+            var distance = Utilities.CalculateDistance(organ, resourceNode);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestOrgan = organ;
+            }
+        }
+
+        return closestOrgan;
+    }
+
+    public Node[] GetNodesItCanGrowInto(BaseNode node)
+    {
+        var adjacentPositions = new (int X, int Y)[]
+        {
+            (node.X + 1, node.Y),
+            (node.X - 1, node.Y),
+            (node.X, node.Y + 1),
+            (node.X, node.Y - 1)
+        };
+
+        var growableNodes = new List<Node>();
+
+        foreach (var pos in adjacentPositions)
+        {
+            if (pos.X < 0 || pos.X >= _arena.WIDTH || pos.Y < 0 || pos.Y >= _arena.HEIGHT)
+                continue;
+
+            var index = pos.Y * _arena.WIDTH + pos.X;
+            var gridNode = _arena.Grid[index];
+            if (gridNode.Type == NodeType.EMPTY || gridNode.Type == NodeType.PROTEIN_SOURCE)
+                growableNodes.Add(gridNode);
+        }
+
+        return growableNodes.ToArray();
+    }
+
+    public Node[] SortByClosesToNode(BaseNode node)
+    {
+        return _arena.Grid.OrderBy(n => Utilities.CalculateDistance(n, node)).ToArray();
+    }
+
+    public ActionPlayer AggresiveStratey()
+    {
+        var nodesICanGrowInto = _arena.GetNodesICanGrowInto();
+        var nodes = _arena.SortByDistanceToNodesICanGrowInto(nodesICanGrowInto);
+
+        DebugLog.Log($"nodes I can grow into: {nodes.Length}");
+        DebugLog.Log($"Actions slots available: {_player.ActionSlotSlot.Count}");
+
+        for (var i = 0; i < _player.ActionSlotSlot.Count; i++)
+        {
+            if (!DoIHaveEnoughResourcesForGrowth()) break;
+            DebugLog.Log($"I have enough Resources for growth. Processing action slot {i}");
+
+            var actionSlot = _player.ActionSlotSlot[i];
+            var targetNode = nodesICanGrowInto.FirstOrDefault();
+
+            DebugLog.Log($"targetNode {targetNode}");
+
+            if (targetNode == null) continue;
+
+            var growFromEntity = GetClosestOrganToNode(targetNode)?.Occupier;
+
+            DebugLog.Log($"sourceEntity {growFromEntity}");
+
+            if (growFromEntity == null) continue;
+
+            actionSlot.Action = new GrowAction(growFromEntity.OrganId, targetNode.X, targetNode.Y, EntityType.BASIC);
+        }
+
+
+        return _player;
+    }
+
+    public ActionPlayer AggresiveStrateyV2()
+    {
+        var closestResourceNode = GetClosestResourceNodeToAnyOfMyOrgans();
+        if (closestResourceNode == null) return _player;
+
+        DebugLog.Log($"Closest resource node: {closestResourceNode}");
+
+        var sourceOrgan = GetClosestOrganToResourceNode(closestResourceNode);
+        DebugLog.Log($"Source organ to grow from: {sourceOrgan}");
+
+        var nodesItCanGrowInto = GetNodesItCanGrowInto(sourceOrgan);
+
+
+        DebugLog.Log($"nodes I can grow into from source organ: {nodesItCanGrowInto.Length}");
+
+        var sortedNodes = SortByClosesToNode(closestResourceNode);
+
+        for (var i = 0; i < _player.ActionSlotSlot.Count; i++)
+        {
+            if (!DoIHaveEnoughResourcesForGrowth()) break;
+            DebugLog.Log($"I have enough Resources for growth. Processing action slot {i}");
+            var actionSlot = _player.ActionSlotSlot[i];
+            var targetNode = sortedNodes.FirstOrDefault();
+            DebugLog.Log($"targetNode {targetNode}");
+            if (targetNode == null) continue;
+            actionSlot.Action = new GrowAction(sourceOrgan.OrganId, targetNode.X, targetNode.Y, EntityType.BASIC);
+        }
+
+        return _player;
+    }
+}
+
 public class ActionPlayer
 {
-    private readonly Action[] actions;
+    private readonly ActionSlot[] actionSlots;
 
     public ActionPlayer(int requiredActionsCount)
     {
-        actions = new Action[requiredActionsCount];
-        for (var i = 0; i < requiredActionsCount; i++) actions[i] = new WaitAction();
+        actionSlots = new ActionSlot[requiredActionsCount];
+        for (var i = 0; i < requiredActionsCount; i++) actionSlots[i] = new ActionSlot { Action = new WaitAction() };
     }
 
-    public IReadOnlyList<Action> Actions => actions;
-
-    public void SetAction(int index, Action action)
-    {
-        if (index >= 0 && index < actions.Length && action != null) actions[index] = action;
-    }
+    public IReadOnlyList<ActionSlot> ActionSlotSlot => actionSlots.AsReadOnly();
 
     public void Play()
     {
-        foreach (var action in actions) action.Play();
+        foreach (var actionSlot in actionSlots) actionSlot.Action.Play();
     }
 }
+
 
 /**
  * Grow and multiply your organisms to end up larger than your opponent.
  */
-internal class Game
+public class Game
 {
     private static Arena arena;
     private static Resources MyResources;
@@ -257,6 +585,7 @@ internal class Game
         inputs = Console.ReadLine().Split(' ');
         var width = int.Parse(inputs[0]); // columns in the game grid
         var height = int.Parse(inputs[1]); // rows in the game grid
+        arena = new Arena(width, height);
     }
 
     private static void Main(string[] args)
@@ -269,11 +598,14 @@ internal class Game
         while (true)
         {
             var entities = ParseEntities();
+            arena.UpdateArenaWithEntities(entities);
             ParseResources();
+
             var requiredActionsCount =
                 int.Parse(Console.ReadLine()); // your number of organisms, output an action for each one in any order
 
             var actionPlayer = new ActionPlayer(requiredActionsCount);
+            actionPlayer = new Strategies(MyResources, OppResources, arena, actionPlayer).AggresiveStrateyV2();
             actionPlayer.Play();
         }
     }
